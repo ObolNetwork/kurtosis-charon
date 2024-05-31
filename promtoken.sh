c3750dcca5f6c64336b967f3087ce0ec4376f11c
Gianguido Sorà <gsora@users.noreply.github.com>
2024-05-31 12:08:14 +0200

    promtoken.sh: allow setting custom prometheus write token
    If PROMETHEUS_WRITE_URL is set, replace it as the default write url.
    
    Do not exit with error if no environment variables are set.

--- promtoken.sh
+++ promtoken.sh
@@ -6,20 +6,28 @@ then
   exit 1
 fi
 
-# Check if PROMETHEUS_REMOTE_WRITE_TOKEN environment variable is set
-if [ -z "$PROMETHEUS_REMOTE_WRITE_TOKEN" ]; then
-  echo "Error: PROMETHEUS_REMOTE_WRITE_TOKEN environment variable is not set."
-  exit 1
-fi
-
-# Read the PROMETHEUS_REMOTE_WRITE_TOKEN from the environment variable
-token=$PROMETHEUS_REMOTE_WRITE_TOKEN
-
 # Load the YAML configuration file into a variable
 config=$(cat prometheus/prometheus.yml)
 
-# Replace the credentials value in the config with the token
-updated_config=$(echo "$config" | sed "s|credentials: \"\"|credentials: \"$token\"|")
+updated_config=$(echo "$config")
+
+# Check if PROMETHEUS_REMOTE_WRITE_TOKEN environment variable is set
+# if it's set, replace the it in the final configuration
+if ! [ -z "$PROMETHEUS_REMOTE_WRITE_TOKEN" ]; then
+  # Read the PROMETHEUS_REMOTE_WRITE_TOKEN from the environment variable
+  token=$PROMETHEUS_REMOTE_WRITE_TOKEN
+
+  # Replace the credentials value in the config with the token
+  updated_config=$(echo "$updated_config" | sed "s|credentials: \"\"|credentials: \"$token\"|")
+fi
+
+# Check if PROMETHEUS_WRITE_URL environment variable is set
+# if it's set, replace the URL with the final configuration
+if ! [ -z "$PROMETHEUS_WRITE_URL" ]; then
+  url=$PROMETHEUS_WRITE_URL
+
+  updated_config=$(echo "$updated_config" | sed "s|url: https://vm.monitoring.gcp.obol.tech/write|url: $url|g")
+fi
 
 # Write the updated configuration back to the YAML file
 echo "$updated_config" > prometheus/prometheustmp.yml
